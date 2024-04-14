6caab37799c17cfb3c4bb0e2acd17dc03e4700b1
PauloAlves8039 <pj38alves@gmail.com>
2024-04-14 20:04:43 -0300

    feat: implementação do contexto com banco de dados

--- RangoAgil.API/Program.cs
+++ RangoAgil.API/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using RangoAgil.API.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<RangoDbContext>(options =>
+    options.UseSqlite(builder.Configuration["ConnectionStrings:RangoDbConnectionString"])
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
