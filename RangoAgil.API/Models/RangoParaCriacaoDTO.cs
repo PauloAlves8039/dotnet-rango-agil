e0b89a27153c94f091bf1272c269b86b1000ae7d
PauloAlves8039 <pj38alves@gmail.com>
2024-04-18 20:41:17 -0300

    feat: adição das classes dtos

---
+++ RangoAgil.API/Models/RangoParaCriacaoDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RangoAgil.API.Models;
+
+public class RangoParaCriacaoDTO
+{
+    [Required]
+    [StringLength(100, MinimumLength = 3)]
+    public required string Nome { get; set; }
+}
