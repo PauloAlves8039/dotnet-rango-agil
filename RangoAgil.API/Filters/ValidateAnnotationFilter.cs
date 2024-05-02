4237377ae1ebe2816596cdf2938e38624ac09966
PauloAlves8039 <pj38alves@gmail.com>
2024-05-02 20:06:54 -0300

    feat: criação dos filtros

---
+++ RangoAgil.API/Filters/ValidateAnnotationFilter.cs
@@ -0,0 +1,19 @@
+﻿using MiniValidation;
+using RangoAgil.API.Models;
+
+namespace RangoAgil.API.Filters;
+
+public class ValidateAnnotationFilter : IEndpointFilter
+{
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        var rangoParaCriacaoDTO = context.GetArgument<RangoParaCriacaoDTO>(2);
+
+        if (!MiniValidator.TryValidate(rangoParaCriacaoDTO, out var validationErros)) 
+        {
+            return TypedResults.ValidationProblem(validationErros);
+        }
+
+        return await next(context);
+    }
+}
